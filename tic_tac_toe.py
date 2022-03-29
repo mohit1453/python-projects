a8dd7e8684c34d401c501df2ab9519346d96f306
mohit1453 <67677509+mohit1453@users.noreply.github.com>
2022-03-29 19:52:49 +0530

    Create tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,46 @@
+board=[" " for x in range(0,9)]
+def print_board():
+    row1="|{}|{}|{}|".format(board[0],board[1],board[2])
+    row2="|{}|{}|{}|".format(board[3],board[4],board[5])
+    row3="|{}|{}|{}|".format(board[6],board[7],board[8])
+    
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+    
+def player_move(icon):
+    if icon=="X":
+        number=1
+    else:
+        number=2
+    print("Your Turn Player {} ".format(number))
+    choice=int(input("Enter Your move in 1-9  : ").strip())
+    if board[choice-1]==" ":
+          board[choice-1]=icon
+    else:
+        print()
+        print("Place Already Taken!")
+def isvictory(icon):
+    if (board[0]==icon and board[1]==icon and board[2]==icon)or(board[3]==icon and board[4]==icon and board[5]==icon)or(board[6]==icon and board[7]==icon and board[8]==icon)or(board[0]==icon and board[3]==icon and board[6]==icon)or(board[1]==icon and board[4]==icon and board[7]==icon)or(board[2]==icon and board[5]==icon and board[8]==icon)or(board[0]==icon and board[4]==icon and board[8]==icon)or(board[2]==icon and board[4]==icon and board[6]==icon):
+        return True
+    else:
+        return False
+        
+    
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+    if isvictory("X"):
+        print("X! Wins Congratulations!")
+        break
+    player_move("O")
+    
+    if isvictory("O"):
+        print("O! Wins Congratulations!")
+        break
+  
+    
+    
